0f77d3c16fbdefba2080b44eb06a069c2da8bb5c
Jesse Bellingham <jbellingham91@gmail.com>
2022-02-15 21:34:44 +1100

    Added topic model and create form

---
+++ discuss/lib/discuss/models/topic.ex
@@ -0,0 +1,16 @@
+defmodule Discuss.Topic do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "topics" do
+    field :title, :string
+  end
+
+  @doc false
+  def changeset(topic, attrs \\ %{}) do
+    # \\ %{} is default value syntax. If nil is passed for attrs, default to an empty map
+    topic
+    |> cast(attrs, [:title])
+    |> validate_required([:title])
+  end
+end
